8302853b2177193b9f9fea0b1d5a26e22062a097
Roe Greene <robert@byrojo.com>
2022-07-04 18:56:50 -0700

    add settings

--- wp-content/plugins/react-ka-ching-wp-plugin/react-ka-ching-wp-plugin.php
+++ wp-content/plugins/react-ka-ching-wp-plugin/react-ka-ching-wp-plugin.php
@@ -20,6 +20,7 @@ if (!class_exists('reactkachingwpplugin')) {
 
         function init()
         {
+            register_deactivation_hook(__FILE__, [$this, 'uninstall']);
             register_activation_hook(__FILE__, [$this, 'install']);
             register_activation_hook(__FILE__, [$this, 'setConfig']);
             add_action('the_content', [$this, 'my_thank_you_text']);
@@ -42,18 +43,28 @@ if (!class_exists('reactkachingwpplugin')) {
             $this->setupDatabase();
         }
 
+        function uninstall()
+        {
+            $this->dropDatabase();
+        }
+
+        function dropDatabase()
+        {
+            $sql = "DROP TABLE " . $this->tableName();
+            $this->wpdb()->query($sql);
+        }
+
         function setupDatabase()
         {
             $charset_collate = $this->wpdb()->get_charset_collate();
-
-            $sql = "CREATE TABLE $this->tableName() (
-                        key tinytext NOT NULL UNIQUE,
-                        value text NOT NULL
-                        PRIMARY KEY (key)
+            $tableName = $this->tableName();
+            $sql = "CREATE TABLE $tableName (
+                        key_name text NOT NULL,
+                        key_value text NOT NULL,
+                        CONSTRAINT rkc_key PRIMARY KEY (key_name(255))
                         ) $charset_collate;";
 
-            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-            dbDelta($sql);
+            $this->wpdb()->query($sql);
         }
 
         function setConfig()
@@ -61,28 +72,67 @@ if (!class_exists('reactkachingwpplugin')) {
             $this->wpdb()->insert(
                 $this->tableName(),
                 array(
-                    'key' => "config",
-                    'value' => "{}",
+                    'key_name' => "config",
+                    'key_value' => "{}",
                 )
             );
         }
 
+        function encode_decode($stringToHandle = "", $encrypt = true)
+        {
+            // Set default output value
+            $output = null;
+            // Set secret keys
+            $secret_key = wp_salt();
+            $key = hash('sha256', $secret_key);
+            $iv = substr(hash('sha256', $secret_key), 0, 16);
+            // Check whether encryption or decryption
+            if ($encrypt) {
+                // We are encrypting
+                $output = base64_encode(openssl_encrypt($stringToHandle, "AES-256-CBC", $key, 0, $iv));
+            } else {
+                // We are decrypting
+                $output = openssl_decrypt(base64_decode($stringToHandle), "AES-256-CBC", $key, 0, $iv);
+            }
+            // Return the final value
+            return $output;
+        }
         function updateConfig()
         {
             if (isset($_POST['react_ka_ching_settings_form_nonce']) && wp_verify_nonce($_POST['react_ka_ching_settings_form_nonce'], 'react_ka_ching_settings_form_nonce')) {
+                $wpUser = sanitize_user($_POST["wpUser"]);
+                $wpPass = sanitize_text_field($_POST["wpPass"]);
+                $wpStatus = sanitize_text_field($_POST["wpStatus"]);
+                $amazonAccessKey = sanitize_key($_POST["amazonAccessKey"]);
+                $amazonSecretKey = sanitize_key($_POST["amazonSecretKey"]);
+                $amazonPartnerTag = sanitize_text_field($_POST["amazonPartnerTag"]);
+                $amazonHost = sanitize_text_field($_POST["amazonHost"]);
+                $amazonRegion = sanitize_text_field($_POST["amazonRegion"]);
 
-                // sanitize the input
-                // $nds_user_meta_key = sanitize_key($_POST['nds']['user_meta_key']);
-                // $nds_user_meta_value = sanitize_text_field($_POST['nds']['user_meta_value']);
-                // $nds_user =  get_user_by('login',  $_POST['nds']['user_select']);
-                // $nds_user_id = absint($nds_user->ID);
+                $json = json_encode([
+                    "wpUser" => $wpUser,
+                    "wpPass" => $this->encode_decode($wpPass),
+                    "wpStatus" => $wpStatus,
+                    "amazonAccessKey" => $this->encode_decode($amazonAccessKey),
+                    "amazonSecretKey" => $this->encode_decode($amazonSecretKey),
+                    "amazonPartnerTag" => $amazonPartnerTag,
+                    "amazonHost" => $amazonHost,
+                    "amazonRegion" => $amazonRegion
+                ]);
 
-                // // do the processing
+                $this->wpdb()->update(
+                    $this->tableName(),
+                    [
+                        "key_value" => $json
+                    ],
+                    [
+                        "key_name" => "config"
+                    ]
+                );
 
-                // // add the admin notice
-                // $admin_notice = "success";
-
-                // redirect the user to the appropriate page
+                wp_redirect(admin_url('admin.php') . "?page=" . $this->plugin_name());
+                exit;
+            } else {
                 wp_redirect(admin_url('admin.php') . "?page=" . $this->plugin_name());
                 exit;
             }
@@ -95,6 +145,7 @@ if (!class_exists('reactkachingwpplugin')) {
 
         function render_admin_panel()
         {
+            $row = $this->wpdb()->get_row("SELECT * FROM " . $this->tableName() . " WHERE key_name='config'");
             include(sprintf("%s/views/admin.php", dirname(__FILE__)));
         }
 
--- wp-content/plugins/react-ka-ching-wp-plugin/views/admin.php
+++ wp-content/plugins/react-ka-ching-wp-plugin/views/admin.php
@@ -3,7 +3,7 @@
     Renders the admin panel
 */
 $react_ka_ching_add_meta_nonce = wp_create_nonce('react_ka_ching_settings_form_nonce');
-
+$adminSettings = json_decode($row->key_value);
 ?>
 
 <div class="react-ka-ching-app">
@@ -16,42 +16,45 @@ $react_ka_ching_add_meta_nonce = wp_create_nonce('react_ka_ching_settings_form_n
         <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post" id="react_ka_ching_settings_form" autocomplete="off">
             <input type="hidden" name="action" value="react_ka_ching_form_response">
             <input type="hidden" name="react_ka_ching_settings_form_nonce" value="<?php echo $react_ka_ching_add_meta_nonce ?>" />
-
+            <!--hack to remove auto user and password -->
+            <input type="password" tabindex="-1" style="position: fixed; z-index: 0; opacity: 0; width: 0; height: 0; left: -10000px;" name="pass_hack"></input>
+            <input type="username" tabindex="-1" style="position: fixed; z-index: 0; opacity: 0; width: 0; height: 0; left: -10000px;"></input>
+            <!-- end hack -->
             <fieldset>
                 <label>WordPress username</label>
-                <input type="text" name="wp-user"></input>
+                <input type="text" name="wpUser" value="<?php echo $adminSettings->wpUser; ?>"></input>
             </fieldset>
             <fieldset>
                 <label>WordPress user password</label>
-                <input type="password" name="wp-pass"></input>
+                <input type="password" name="wpPass" value="<?php echo $this->encode_decode($adminSettings->wpPass, false); ?>"></input>
             </fieldset>
             <fieldset>
                 <label>WordPress default post status</label>
-                <select name="wp-status">
+                <select name="wpStatus">
                     <option></option>
-                    <option value="publish">Publish</option>
-                    <option value="draft">Draft</option>
+                    <option value="publish" <?php echo $adminSettings->wpStatus === "publish" ? "selected" : ""; ?>>Publish</option>
+                    <option value="draft" <?php echo $adminSettings->wpStatus === "draft" ? "selected" : ""; ?>>Draft</option>
                 </select>
             </fieldset>
             <fieldset>
                 <label>Amazon Access Key</label>
-                <input type="password" name="amazon-access-key"></input>
+                <input type="password" name="amazonAccessKey" value="<?php echo $this->encode_decode($adminSettings->amazonAccessKey, false); ?>"></input>
             </fieldset>
             <fieldset>
                 <label>Amazon Secret Key</label>
-                <input type="password" name="amazon-secret-key"></input>
+                <input type="password" name="amazonSecretKey" value="<?php echo $this->encode_decode($adminSettings->amazonSecretKey, false); ?>"></input>
             </fieldset>
             <fieldset>
                 <label>Amazon Partner Tag</label>
-                <input type="text" name="amazon-partner-tag"></input>
+                <input type="text" name="amazonPartnerTag" value="<?php echo $adminSettings->amazonPartnerTag; ?>"></input>
             </fieldset>
             <fieldset>
                 <label>Amazon Host</label>
-                <input type="text" name="amazon-host" value="webservices.amazon.com"></input>
+                <input type="text" name="amazonHost" value="<?php echo $adminSettings->amazonHost ?  $adminSettings->amazonHost : "webservices.amazon.com" ?>"></input>
             </fieldset>
             <fieldset>
                 <label>Amazon Region</label>
-                <input type="text" name="amazon-region" value="us-east"></input>
+                <input type="text" name="amazonRegion" value="<?php echo $adminSettings->amazonRegion ?  $adminSettings->amazonRegion : "us-east-1" ?>"></input>
             </fieldset>
             <br />
             <input type="submit" value="Save"></input>
